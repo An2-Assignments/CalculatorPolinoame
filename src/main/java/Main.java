28ffc5b147ab5f0a5c7e350ddb036b65d8b748f0
Orsan Tudor <tudor.orsan@student.utcluj.ro>
2021-02-24 20:11:14 +0200

    add new class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("Terminare program.");
+        System.out.println("Testare pentru update.");
     }
 }
